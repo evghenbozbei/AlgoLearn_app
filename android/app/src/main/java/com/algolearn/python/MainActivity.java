86da41789090b629b0302d58440313100a02ce14
Evgheni Bozbei <bozbei.e@gmail.com>
2026-09-02 10:10:59 +0300

    feat(android): add Capacitor Android app

---
+++ android/app/src/main/java/com/algolearn/python/MainActivity.java
@@ -0,0 +1,5 @@
+package com.algolearn.python;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.algolearn.python',
+  appName: 'AlgoLearn Python',
+  webDir: 'dist'
+};
+
+export default config;
